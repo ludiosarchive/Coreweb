9b03b91b114cf02350eaee5bf35f8c90485245c1
ivank <ivank@ludios.net>
2009-11-28 00:56:43 -0800

    be more like other start_dev.sh's

--- start_dev.sh
+++ start_dev.sh
@@ -3,4 +3,6 @@ export JSPATH="$HOME/JSPATH"
 export PYTHONPATH=$HOME/Projects/Coreweb
 export PYRELOADING=1
 
+echo "Using `which twistd`"
+
 looper twistd -no cwrun -p CW.Test -a tcp:9090:interface=0 -b ssl:443:privateKey=dev_keys/x.linuxwan.com-key.pem:interface=0
