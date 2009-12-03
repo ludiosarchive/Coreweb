fbbaba9148ce30eac40c5daff32b196add8e6e02
ivank <ivank@ludios.net>
2009-12-02 18:20:33 -0800

    [PATCH 01/42] Import hxJson2 0.8

---
+++ hxJson2/hxjson2/JSONToken.hx
@@ -0,0 +1,98 @@
+/*
+  hxjson2 by Philipp Klose (philipp.klose@byteanvil.com)
+  
+  Ported from as3corelib (http://code.google.com/p/as3corelib/)
+  
+  com.adobe.serialization.JSONToken
+  
+  Original source code by:
+   
+  Copyright (c) 2008, Adobe Systems Incorporated
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without 
+  modification, are permitted provided that the following conditions are
+  met:
+
+  * Redistributions of source code must retain the above copyright notice, 
+    this list of conditions and the following disclaimer.
+  
+  * Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the 
+    documentation and/or other materials provided with the distribution.
+  
+  * Neither the name of Adobe Systems Incorporated nor the names of its 
+    contributors may be used to endorse or promote products derived from 
+    this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+  package hxjson2;
+
+	class JSONToken {
+	
+		public var type(gettype, settype):Int;
+		public var value(getvalue, setvalue):Dynamic;
+		private var _type:Int;
+		private var _value:Dynamic;
+		
+		/**
+		 * Creates a new JSONToken with a specific token type and value.
+		 *
+		 * @param type The JSONTokenType of the token
+		 * @param value The value of the token
+		 */
+		public function new( type:Int = -1 /* JSONTokenType.UNKNOWN */, value:Dynamic = null ) {
+			_type = type;
+			_value = value;
+		}
+		
+		/**
+		 * Returns the type of the token.
+		 *
+		 * @see com.adobe.serialization.json.JSONTokenType
+		 */
+		public function gettype():Int {
+			return _type;	
+		}
+		
+		/**
+		 * Sets the type of the token.
+		 *
+		 * @see com.adobe.serialization.json.JSONTokenType
+		 */
+		public function settype( value:Int ):Int {
+			_type = value;	
+			return value;
+		}
+		
+		/**
+		 * Gets the value of the token
+		 *
+		 * @see com.adobe.serialization.json.JSONTokenType
+		 */
+		public function getvalue():Dynamic {
+			return _value;	
+		}
+		
+		/**
+		 * Sets the value of the token
+		 *
+		 * @see com.adobe.serialization.json.JSONTokenType
+		 */
+		public function setvalue ( v:Dynamic ):Void {
+			_value = v;	
+		}
+
+	}
