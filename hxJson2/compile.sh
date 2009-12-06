e0a4dc8aec13a1c0bff49f2bad4ea2aad7f9b89e
ivank <ivank@ludios.net>
2009-12-05 23:48:00 -0800

    [PATCH 41/42] rm compile.sh (now it's only in Coreweb); move LICENSE

--- hxJson2/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh -e
-export HAXE_LIBRARY_PATH=/usr/local/haxe/std:hxJson2
-export HAXE_HOME=/usr/local/haxe
-PATH=$PATH:$HAXE_LIBRARY_PATH:$HAXE_HOME/bin
-date > ./build.log
-echo >> ./build.log
-haxe -v -swf test9.swf -swf-version 9 -swf-header 800:600:1:F7F7F7 -main cw.json.Tests >> ./build.log
