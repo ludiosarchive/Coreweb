b73b712a25e3df03b1cfd9684f9574a1e46b4cae
ivank <ivank@ludios.net>
2009-12-05 21:52:00 -0800

    add flash/OBSOLETE-STUFF-HERE marker; make js/CW/firebugEmptyObjects.js faster

--- js/CW/firebugEmptyObjects.js
+++ js/CW/firebugEmptyObjects.js
@@ -3,9 +3,9 @@
 		var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
 		"group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
 
-		window.console = {};
-		for (var i = 0; i < names.length; ++i) {
-			window.console[names[i]] = function() {};
+		var n = names.length;
+		while(n--) {
+			window.console[names[n]] = function() {};
 		}
 	}
 })();
