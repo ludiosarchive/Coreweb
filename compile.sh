cbe0169b55a60110b6283ef6e6c4648b3819680c
ivank <ivank@ludios.net>
2009-12-06 00:00:12 -0800

    update README; update compile.sh for incoming hxJson2

--- compile.sh
+++ compile.sh
@@ -2,7 +2,12 @@
 export HAXE_LIBRARY_PATH=/usr/local/haxe/std:./hx
 export HAXE_HOME=/usr/local/haxe
 PATH=$PATH:$HAXE_LIBRARY_PATH:$HAXE_HOME/bin
+
 date > ./cwtools/exp/build_Experiments.log
 echo >> ./cwtools/exp/build_Experiments.log
 haxe -v -swf-version 9 -swf ./cwtools/exp/Experiments.swf -main cw.exp.Experiments >> ./cwtools/exp/build_Experiments.log
 cp /med/builds/swfobject/swfobject/expressInstall.swf ./cwtools/exp/
+
+date > ./cwtools/exp/build_json.log
+echo >> ./cwtools/exp/build_json.log
+haxe -v -swf ./cwtools/exp/JSON.swf -swf-version 9 -swf-header 800:600:1:F7F7F7 -main cw.json.Tests >> ./cwtools/exp/build_json.log
