770f168fbb9bb962a3c40061b26bc0d46aa7a6d6
ivank <ivank@ludios.net>
2009-12-15 01:02:07 -0800

    remove CW.JSON and tests; use goog.json instead

--- js/CW/Test/TestBase.js
+++ js/CW/Test/TestBase.js
@@ -4,63 +4,6 @@
 
 // import CW.UnitTest
 
-CW.UnitTest.TestCase.subclass(CW.Test.TestBase, 'JSONTests').methods(
-
-	// if ever need some kind of "bootstrap" we'll add it back
-//	/**
-//	 * Verify that the CW module's bootstrap function sets its '_location'
-//	 * attribute.
-//	 */
-//	function test_divmodBootstrap(self) {
-//		var notCW = {};
-//		notCW.bootstrap = CW.bootstrap;
-//		var STUFF = "hello there";
-//		notCW.bootstrap(STUFF);
-//		self.assertIdentical(notCW._location, STUFF);
-//	},
-
-	/**
-	 * Assert that JSON encoder is doing at least basic escaping.
-	 */
-	function test_basicEscaping(self) {
-
-		// NULL is the only character we really care about being encoded,
-		// because there may be problems uploading it through XHR or Flash.
-
-		// simplejson on the server will liberally decode just about anything.
-		// (control characters like RTL are unlikely to cause problems. TODO: confirm?)
-
-		var s = '\r\n\f\b\t\u0000';
-		var repr = CW.JSON.stringify(s);
-
-		var expected = '"\\r\\n\\f\\b\\t\\u0000"';
-
-		// Firefox 3.1 does this, but OK if other browsers do the same thing. (Two answers are valid here.)
-		var expectedFF31Plus = '"\\u000d\\u000a\\u000c\\u0008\\u0009\\u0000"';
-		if(repr.length > expected.length) {
-			self.assertIdentical(repr, expectedFF31Plus);
-		} else {
-			self.assertIdentical(repr, expected);
-		}
-	},
-
-
-	/**
-	 * Trivial JSON serialization test that doesn't rely on some
-	 * arbitrary internal JS object property order.
-	 */
-	function test_serializeJSON(self) {
-		var expr = [{}, {1: "2"}, {"c": [null, NaN, [{x: ["\\", "'", ""]}]]}];
-		var json = CW.JSON.stringify(expr);
-
-		// We assume that if the browser has a native JSON encoder,
-		// it won't be adding spaces. (FF 3.1 behavior seems to match this)
-		var expected = '[{},{"1":"2"},{"c":[null,null,[{"x":["\\\\","\'",""]}]]}]';
-		self.assertIdentical(json, expected);
-	}
-);
-
-
 
 CW.UnitTest.TestCase.subclass(CW.Test.TestBase, 'ArraysEqualTests').methods(
 	/**
