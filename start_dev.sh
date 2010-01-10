b8742bce64cda2c3fad335d7cf7cc8471dd510a8
ivank <ivank@ludios.net>
2010-01-09 19:26:00 -0800

    start_dev.sh: include ~/Projects/Webmagic in PYTHONPATH

--- start_dev.sh
+++ start_dev.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 export JSPATH="$HOME/JSPATH"
-export PYTHONPATH=$HOME/Projects/Coreweb
+export PYTHONPATH=$HOME/Projects/Coreweb:$HOME/Projects/Webmagic
 export PYRELOADING=1
 
 echo "Using `which twistd`"
