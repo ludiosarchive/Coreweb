8cb4524a6623b5cb6d7411a1f3a697624abca75a
ivank <ivank@ludios.net>
2010-01-29 18:03:31 -0800

    start with python -N instead of -OOOOO

--- start_dev.sh
+++ start_dev.sh
@@ -5,4 +5,4 @@ export PYRELOADING=1
 
 echo "Using `which twistd`"
 
-looper python -OOOOO `which twistd` -no cwrun -p cw.Test -a tcp:9090:interface=0 -b ssl:443:privateKey=dev_keys/x.linuxwan.com-key.pem:interface=0
+looper python -N `which twistd` -no cwrun -p cw.Test -a tcp:9090:interface=0 -b ssl:443:privateKey=dev_keys/x.linuxwan.com-key.pem:interface=0
