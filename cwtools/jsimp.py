330ed9ff4a68b8a6ac5a01df16a84239048ab6ed
ivank <ivank@ludios.net>
2010-02-27 00:11:23 -0800

    Move JavaScriptWriter to cwtools/sandbox; describe why it is obsolete (use normal `if` statements and Closure Compiler instead)

--- cwtools/jsimp.py
+++ cwtools/jsimp.py
@@ -594,50 +594,5 @@ class VirtualScript(_BaseScript):
 
 
 
-class JavaScriptWriter(object):
-	"""
-	The macro language defined here should be easily portable to another
-	template system / language (maybe TeX?).
-
-	When writing things for this template system, make it sound like English:
-
-		browser.supports('JSON')
-
-		browser.hasBug('')
-	"""
-
-	def __init__(self):
-
-		# These are chosen very carefully so that JS syntax-highlights reasonably
-		# even with these ugly macros.
-
-		self.env = jinja2.Environment(
-			line_statement_prefix = '//]',
-			variable_start_string = '/***/',
-			variable_end_string = '//',
-			# TODO: ? also define block_(end|start)_string
-			comment_start_string = '/*###',
-			comment_end_string = '*/',
-		)
-
-
-	def render(self, template, dictionary={}):
-		"""
-		C{template} is the unicode (or str) template.
-		C{dictionary} is passed to jinja2 for variables in the template.
-
-		@rtype: unicode
-		@return: the rendered template
-		"""
-		rendered = self.env.from_string(template).render(dictionary)
-		# jinja2 forgets about how many newlines there should be at the end, or something
-		if not rendered.endswith(u'\n'):
-			rendered += u'\n'
-		return rendered
-
-
-_theWriter = JavaScriptWriter()
-
-
 from pypycpyo import optimizer
 optimizer.bind_all_many(vars(), _postImportVars)
