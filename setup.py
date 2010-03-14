6eeb4f5da48c08f85f319441b086aab510151280
ivank <ivank@ludios.net>
2010-03-14 11:23:13 -0700

    Expose a __version__ as per http://jcalderone.livejournal.com/54911.html 's advice.

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@
 
 from distutils.core import setup
 
+import cwtools
+
 setup(
 	name='Coreweb',
-	version='10.2.23',
+	version=cwtools.__version__,
 	description="Coreweb",
 	packages=['cwtools', 'cwtools.test', 'twisted.plugins'],
 	package_data={'cwtools': ['*.html', 'testres/*', 'exp/*'],},
