c92493054197ef13f434ce570b33b40139347741
ivank <ivank@ludios.net>
2010-03-22 17:26:51 -0700

    add js/_Compilables.js which goog.require(...)s every module we plan to (possibly) use publicly

---
+++ js/_Compilables.js
@@ -0,0 +1,10 @@
+// in Coreweb
+goog.require('cw.array');
+goog.require('cw.clock');
+goog.require('cw.eventual');
+goog.require('cw.externalinterface');
+goog.require('cw.string');
+goog.require('cw.uri');
+goog.require('cw.whoami');
+// in Minerva
+goog.require('cw.net');
