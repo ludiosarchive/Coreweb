47ecef124d33f26aae2dd5fe8981d55fbbc96a59
ivank <ivank@ludios.net>
2010-05-17 22:05:18 +0000

    js/cw/__init__.js: clarify English

--- js/cw/__init__.js
+++ js/cw/__init__.js
@@ -3,6 +3,6 @@
  */
 
 // This line is useless because it's actually goog.require that creates the
-// necessary `cw` namespace, but does prevent IntelliJ IDEA from displaying
-// squiggly underlines under every usage of `cw.`.
+// necessary `cw` namespace. Its only purpose is to prevent IntelliJ IDEA
+// from displaying squiggly underlines under every usage of `cw.`.
 var cw = cw || {};
