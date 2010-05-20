ed704c6b67ad61f2b0c40a22279cd5433772d54e
ivank <ivank@ludios.net>
2010-05-20 18:35:42 +0000

    js/cw/eq.js: more work-in-progress; completely untested

--- js/cw/eq.js
+++ js/cw/eq.js
@@ -54,7 +54,23 @@ cw.eq.eqArray_ = function(one, two, messages) {
  * @private
  */
 cw.eq.eqObject_ = function(one, two, messages) {
+	for(var prop in one) {
+		if(!(prop in two)) {
+			messages.push('property ' + prop + ' missing on right object');
+			return false;
+		}
+		if(!cw.eq.eqAny_(one[prop], two[prop])) {
+			messages.push('earlier comparisons indicate mismatch at property ' + prop);
+			return false;
+		}
+	}
 
+	for(var prop in two) {
+		if(!(prop in two)) {
+			messages.push('property ' + prop + ' missing on left object');
+		}
+	}
+	return true;
 }
 
 
@@ -65,7 +81,7 @@ cw.eq.eqObject_ = function(one, two, messages) {
  *
  * For more inspiration, see:
  * 	http://philrathe.com/articles/equiv and qunit/testrunner.js
- * 	Closure Library's
+ * 	Closure Library's goog.testing.asserts
  */
 cw.eq.eqAny_ = function(one, two, messages) {
 	var typeOne = goog.typeOf(one);
@@ -75,10 +91,12 @@ cw.eq.eqAny_ = function(one, two, messages) {
 		return one === two;
 
 	} else if(typeof one.equals == 'function') {
-		// We aim to be backwards-compatible with `equals` methods
-		// that do not take a `messages` argument.
-		messages.push('running custom equals function');
+		messages.push('running custom equals function on left object');
 		return one.equals(two, messages);
+
+	} else if(typeof two.equals == 'function') {
+		messages.push('running custom equals function on right object');
+		return two.equals(one, messages);
 
 	} else if(one instanceof RegExp && two instanceof RegExp) {
 		return one.toString() === two.toString();
@@ -89,6 +107,8 @@ cw.eq.eqAny_ = function(one, two, messages) {
 	} else if(typeOne == 'array' && typeTwo == 'array') {
 		messages.push('descending into array');
 		return cw.eq.eqArray_(one, two, messages);
+
+	// TODO: die on objects with __iterator__, like goog.testing.asserts does.
 
 	} else if(typeOne == 'object' && typeTwo == 'object') {
 		messages.push('descending into object');
@@ -108,12 +128,19 @@ cw.eq.NOOP_PUSHABLE_ = {'push': goog.nullFunction};
 
 
 /**
- * Compare any two objects for equality.
- * 
+ * Compare any two objects for deep equality. This properly compares many
+ * built-in types, but also supports the Closure Library-style equality protocol:
+ * If any object or sub-object has an {@code equals} method, it will be used
+ * to check for equality. The {@code equals} method is called with two arguments:
+ * 	#0 the other object
+ * 	#1 an Array into which comparison progress messages are pushed in to.
+ * This is backwards-compatible with {@code equals} methods that take only
+ * 	argument #0.
+ *
  * @param {!Object} one The first object to compare.
  * @param {!Object} two The second object to compare.
- * @param {Array.<string>=} messages Array to push comparison progress
- * 	messages into.
+ * @param {Array.<string>=} messages Array into which comparison progress
+ * 	messages are pushed in to.
  * @return {boolean} Whether the two objects are equal.
  */
 cw.eq.equals = function(one, two, messages) {
