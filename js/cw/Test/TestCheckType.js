6f849241571824c3812d33f02a717fdea67307db
ivank <ivank@ludios.net>
2010-05-22 16:12:53 +0000

    add js/cw/checktype.js and js/cw/Test/TestCheckType.js based on mypy.objops

---
+++ js/cw/Test/TestCheckType.js
@@ -0,0 +1,96 @@
+/**
+ * @fileoverview Tests for cw.checktype
+ */
+
+goog.provide('cw.Test.TestCheckType');
+
+goog.require('cw.Class');
+goog.require('cw.UnitTest');
+goog.require('cw.checktype');
+
+
+// anti-clobbering for JScript
+(function(){
+
+
+/**
+ * Tests for {@code cw.checktype.ensureInt}
+ */
+cw.UnitTest.TestCase.subclass(cw.Test.TestCheckType, 'EnsureIntTests').methods(
+
+	function setUp(self) {
+		self.func = cw.checktype.ensureInt;
+	},
+
+	function test_ensureInt(self) {
+		self.assertIdentical(3, self.func(3));
+		self.assertIdentical(-3, self.func(-3));
+		self.assertIdentical(0, self.func(0));
+		self.assertIdentical(null, self.func(3.5));
+		self.assertIdentical(null, self.func(-3.5));
+		self.assertIdentical(null, self.func(NaN));
+		self.assertIdentical(null, self.func(Infinity));
+		self.assertIdentical(null, self.func(-Infinity));
+		self.assertIdentical(null, self.func(new Number(4)));
+		self.assertIdentical(null, self.func([4]));
+		self.assertIdentical(null, self.func({4: 1}));
+		self.assertIdentical(null, self.func(true));
+		self.assertIdentical(null, self.func(false));
+		self.assertIdentical(null, self.func("4"));
+	}
+);
+
+
+/**
+ * Tests for {@code cw.checktype.ensureIntInRange}
+ *
+ * ensureIntInRange is more restrictive than ensureInt, so we subclass
+ * the ensureInt tests so that we run them as well.
+ */
+cw.Test.TestCheckType.EnsureIntTests.subclass(cw.Test.TestCheckType, 'EnsureIntInRangeTests').methods(
+
+	function setUp(self) {
+		self.func = function(v) {
+			return cw.checktype.ensureInt(v, -10000, 10000);
+		};
+	},
+
+	function test_ensureIntInRange(self) {
+		self.assertIdentical(3, cw.checktype.ensureIntInRange(3, 3, 3));
+		self.assertIdentical(3, cw.checktype.ensureIntInRange(3, -3, 3));
+		self.assertIdentical(-3, cw.checktype.ensureIntInRange(-3, -3, 3));
+		self.assertIdentical(null, cw.checktype.ensureIntInRange(-4, -3, 3));
+		self.assertIdentical(null, cw.checktype.ensureIntInRange(4, -3, 3));
+		
+	}
+);
+
+
+/**
+ * Tests for {@code cw.checktype.ensureBool}
+ */
+cw.UnitTest.TestCase.subclass(cw.Test.TestCheckType, 'EnsureBool').methods(
+
+	function test_ensureBool(self) {
+		self.assertIdentical(true, cw.checktype.ensureBool(true));
+		self.assertIdentical(true, cw.checktype.ensureBool(1));
+		self.assertIdentical(true, cw.checktype.ensureBool(1.0));
+
+		self.assertIdentical(false, cw.checktype.ensureBool(false));
+		self.assertIdentical(false, cw.checktype.ensureBool(0));
+		self.assertIdentical(false, cw.checktype.ensureBool(0.0));
+		self.assertIdentical(false, cw.checktype.ensureBool(-0));
+		self.assertIdentical(false, cw.checktype.ensureBool(-0.0));
+		
+		self.assertIdentical(null, cw.checktype.ensureBool(1.0001));
+		self.assertIdentical(null, cw.checktype.ensureBool(0.0001));
+		self.assertIdentical(null, cw.checktype.ensureBool("0"));
+		self.assertIdentical(null, cw.checktype.ensureBool("1"));
+		self.assertIdentical(null, cw.checktype.ensureBool("true"));
+		self.assertIdentical(null, cw.checktype.ensureBool("false"));
+		self.assertIdentical(null, cw.checktype.ensureBool(NaN));
+	}
+);
+
+
+})(); // end anti-clobbering for JScript
---
+++ js/cw/checktype.js
@@ -0,0 +1,61 @@
+/**
+ * @fileoverview Functions to check the type of objects, based on mypy.objops.
+ */
+
+
+goog.provide('cw.checktype');
+
+goog.require('goog.math');
+
+
+/**
+ * Returns {@code value} is value is a finite integer, else returns {@code null}.
+ *
+ * @param {*} value
+ * @return {?number}
+ */
+cw.checktype.ensureInt = function(value) {
+	if(typeof value == "number" &&
+	goog.math.isInt(/** @type {number} */ (value))) {
+		return value;
+	}
+	return null;
+};
+
+
+/**
+ * Returns {@code value} is value is a finite integer and within inclusive
+ * range {@code [a to b]}, else returns {@code null}.
+ *
+ * @param {*} value
+ * @param {number} min Lower bound allowed for the number.
+ * @param {number} max Upper bound allowed for the number.
+ * @return {?number}
+ */
+cw.checktype.ensureIntInRange = function(value, min, max) {
+	if(typeof value == "number" &&
+	goog.math.isInt(/** @type {number} */ (value)) &&
+	value >= min &&
+	value <= max) {
+		return value;
+	}
+	return null;
+};
+
+
+/**
+ * Convert 1, and True to True.
+ * Convert 0, -0 and False to False.
+ * For all other values, raise L{ValueError}.
+ *
+ * @param {*} value
+ * @return {?bool} boolean equivalent of {@code value}, or {@code null}.
+ */
+cw.checktype.ensureBool = function(value) {
+	if(value === true || value === 1) {
+		return true;
+	} else if(value === false || value === 0) {
+		return false;
+	}
+	return null;
+}
