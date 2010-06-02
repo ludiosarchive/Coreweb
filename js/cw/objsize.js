d25fc4864f4f751ca1a47af3ea34b35695dc843d
ivank <ivank@ludios.net>
2010-06-02 11:54:35 +0000

    js/cw/objsize.js: Improve (hopefully) estimates of object size

--- js/cw/objsize.js
+++ js/cw/objsize.js
@@ -7,17 +7,25 @@ goog.provide('cw.objsize');
 /**
  * Returns a guess of how much memory an object is using.
  *
+ * Estimates of object size are based on what 32-bit CPython 2.7
+ * `sys.getsizeof` returns, which may or may not resemble JavaScript engines.
+ *
  * @param {*} obj Any object.
  * @return {number} Approximately how many bytes of memory the object is using.
  */
 cw.objsize.totalSizeOf = function(obj) {
 	var type = goog.typeOf(obj);
 	if(type == 'string') {
-		// We just hope that the UTF-16 string mostly has 2-byte characters
+		// We don't know if the UTF-16 string has mostly has 2-byte
+		// characters, or 4-byte characters. Split the difference.
 		// and not 4-byte characters.
-		return 64/8 + 2 * obj.length;
-	} else if(type == 'null' || type == 'number' || type == 'boolean' || type == 'undefined') {
-		return 64/8;
+		return 21 + 3 * obj.length;
+	} else if(type == 'number') {
+		return 16;
+	} else if(type == 'boolean') {
+		return 12;
+	} else if(type == 'null' || type == 'undefined') {
+		return 8;
 	} else {
 		// TODO: support arrays
 		throw new Error("cannot determine size of object type " + type);
