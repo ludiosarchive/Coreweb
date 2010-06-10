f549e4535641c00ba05830726b90518824205a39
ivank <ivank@ludios.net>
2010-06-10 11:08:51 +0000

    js/cw/clock.js: update the comment about Opera's JIT problems

--- js/cw/clock.js
+++ js/cw/clock.js
@@ -265,8 +265,10 @@ cw.clock.Clock.prototype.internalAdvance_ = function(extraStopCondition) {
 			// Make sure `this' is the global object for callable (making `this'
 			// "worthless" like it is when the real setTimeout calls you.) Note that
 			// for callable, `this' becomes `window', not `null'.
-			//call.callable_.apply(null, []); // Doesn't work in Opera 10.50
 			call.callable_.call(null);
+			// The equivalent `.apply` call failed in some cases in Opera 10.50-10.60
+			// snapshot, at least before we changed how cw.clock.Clock works.
+			//call.callable_.apply(null, []);
 			// Opera 10.50 has a serious miscompilation issue and strips the
 			// `apply` property on the callable after re-entrant calls happen.
 			// See http://ludios.net/opera_bugs/opera_10_50_reentrant_array.html
