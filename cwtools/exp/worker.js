fc7cb112058721ccb8f5fe8bbbaa25bf71d1e13c
ivank <ivank@ludios.net>
2010-09-23 04:38:28 +0000

    cwtools/exp/worker.js: add comment explaining MessageChannel behavior

--- cwtools/exp/worker.js
+++ cwtools/exp/worker.js
@@ -15,7 +15,13 @@ onconnect = function(e) {
 	} else {
 		slaves.push(port);
 		var channel = new MessageChannel();
-		// TODO: is it safe to send them at the same time?
+
+		// It is safe to do both postMessages without any waiting/confirmation
+		// from one side.  Messages are queued by the browser if the other
+		// port hasn't been listened on yet.  I tested by wrapping the
+		// second postMessage in a setTimeout(..., 5000).  The message
+		// was still received after a delay on Chrome 6.0.472.62 beta and
+		// an Opera 10.70 build on 2010-09-22.
 		port.postMessage('Port for slave to master:', [channel.port1]);
 		master.postMessage('Port for master to slave:', [channel.port2]);
 	}
