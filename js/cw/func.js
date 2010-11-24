efa1515eb2c02cfb410066f2a8b2f145f028a702
ivank <ivank@ludios.net>
2010-11-24 14:28:04 +0000

    js/cw/func.js: isCallable: add return type annotation; improve jsdoc

--- js/cw/func.js
+++ js/cw/func.js
@@ -5,13 +5,14 @@
 goog.provide('cw.func');
 
 /**
- * Determines whether {@code obj} is probably callable.  This works even on
- * objects that IE has proxied because it came from another window (IE proxies
- * objects from other windows but not iframes).
- * @param {*} obj
+ * @param {*} obj Any object.
+ * @return {boolean} Whether {@code obj} is probably callable.  This works
+ * 	even on an object that IE has proxied because it came from another window.
  */
 cw.func.isCallable = function(obj) {
 	return goog.isFunction(obj) || (
+		// IE proxies objects that come from other windows (but not
+		// iframes).
 		typeof obj == 'object' &&
 		goog.isFunction(obj.call) &&
 		goog.isFunction(obj.apply))
