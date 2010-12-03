a1ca29b82ff65f6607d766565f420038f4a5aa53
ivank <ivank@ludios.net>
2010-12-03 23:49:34 +0000

    Delete runtests.sh; add quicktest.sh

---
+++ quicktest.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -e
+
+python -N `which trial` cwtools
--- runtests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh -e
-
-python -N `which trial` cwtools
-trial cwtools
-
-#echo
-#echo "Now running with the Python test runner..."
-#python -W all -m unittest discover
