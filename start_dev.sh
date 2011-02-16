04f23bc67f01f8519a9ca3729239e5ad9d2adb98
ivank <ivan@ludios.org>
2011-02-16 18:17:01 +0000

    start_dev.sh: change $INTERFACE.
    (This file should be fixed or no longer versioned)

--- start_dev.sh
+++ start_dev.sh
@@ -2,7 +2,7 @@
 export JSPATH="$HOME/JSPATH"
 export PYTHONPATH=$HOME/Projects/Coreweb:$HOME/Projects/Webmagic:$HOME/Projects/Ecmaster
 export PYRELOADING=1
-export INTERFACE="192.168.0.119"
+export INTERFACE="192.168.213.133"
 
 echo "Using `which twistd`"
 
