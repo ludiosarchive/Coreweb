949388b6b542c50d64a61d120aa6a16d4e258d11
ivank <ivan@ludios.org>
2011-02-16 18:16:16 +0000

    test_cl.sh: reformat code and use $HOME instead of a specific home
    (This is an obsolete script that should probably be deleted)

--- test_cl.sh
+++ test_cl.sh
@@ -1,1 +1,4 @@
-NODE_BIN="/med/builds/node-v0.1.18/build/default/node" PYTHONPATH="/home/elf/Projects/Coreweb" JSPATH="/home/elf/JSPATH" python cwtools/shelltest.py cw.Test
+export NODE_BIN="/med/builds/node-v0.1.18/build/default/node"
+export PYTHONPATH="$HOME/Projects/Coreweb"
+export JSPATH="$HOME/JSPATH"
+python cwtools/shelltest.py cw.Test
