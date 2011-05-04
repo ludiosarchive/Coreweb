201a4aaf8a38b1a041a4b26ee65709e517bf6cf5
Ivan Kozik <ivan@ludios.org>
2011-05-04 11:42:36 +0000

    hx/cw/exp/Experiments.hx: Document what this is

--- hx/cw/exp/Experiments.hx
+++ hx/cw/exp/Experiments.hx
@@ -1,3 +1,6 @@
+// This is code from my crazy attempt to send Unicode over the
+// ExternalInterface bridge.  Just don't bother and send ASCII instead.
+
 package cw.exp;
 
 import flash.external.ExternalInterface;
