3e07bd245b0d1afb532820725a6ed699c2d66b78
Ivan Kozik <ivan@ludios.org>
2011-05-08 09:54:21 +0000

    corewebsite.py: Remove some unnecessary attributes

--- cwtools/corewebsite.py
+++ cwtools/corewebsite.py
@@ -16,11 +16,6 @@ here = FilePath(cwtools.__path__[0])
 class Compiler(BetterResource):
 	isLeaf = True
 
-	def __init__(self, reactor, JSPATH):
-		self._reactor = reactor
-		self.JSPATH = JSPATH
-
-
 	def render_GET(self, request):
 		ewFile = here.child('compiled').child('_Compilables.js.ew')
 		ewContent = ewFile.getContent()
@@ -71,7 +66,7 @@ class Root(BetterResource):
 		self.putChild('JSPATH', BetterFile(JSPATH.path))
 		self.putChild('exp', htmltools.LiveBox(here.child('exp').path, JSPATH))
 		self.putChild('emptyjs_cached', CachedFile(here.child('exp').child('empty.js').path, JSPATH))
-		self.putChild('compiler', Compiler(reactor, JSPATH))
+		self.putChild('compiler', Compiler())
 		self.putChild('analytics', Analytics(clock=reactor, fsw=None)) # No need for fsw, but this breaks analytics/s/
 		self.putChild('@tests', testing.TestPage(testPackages, JSPATH))
 
