60f63395f11671c882298012b1e38408513d593a
Ivan Kozik <ivan@ludios.org>
2011-05-10 14:04:01 +0000

    build_swfs.sh: don't mess with HAXE_LIBRARY_PATH; don't put timestamps in log files

--- build_swfs.sh
+++ build_swfs.sh
@@ -1,21 +1,18 @@
-#!/bin/zsh -e
-export HAXE_LIBRARY_PATH=/usr/local/haxe/std:./hx
-export HAXE_HOME=/usr/local/haxe
-PATH=$PATH:$HAXE_LIBRARY_PATH:$HAXE_HOME/bin
+#!/bin/sh -e
 
-LOG="./cwtools/exp/build_Experiments.log"
-date > $LOG
-echo >> $LOG
-haxe -v -swf-version 9 -swf ./cwtools/exp/Experiments.swf -main cw.exp.Experiments >> $LOG
-cp ~/Projects/swfobject/swfobject/expressInstall.swf ./cwtools/exp/
+haxe -cp hx -v -swf-version 9 -swf \
+./cwtools/exp/Experiments.swf \
+-main cw.exp.Experiments \
+> ./cwtools/exp/build_Experiments.log
 
-LOG="./cwtools/testres/build_TestExternalInterface.log"
-date > $LOG
-echo >> $LOG
-haxe -v -swf-version 9 -swf ./cwtools/testres/TestExternalInterface.swf -main cw.TestExternalInterface >> $LOG
-cp ~/Projects/swfobject/swfobject/expressInstall.swf ./cwtools/testres/
+haxe -cp hx -v -swf-version 9 -swf \
+./cwtools/testres/TestExternalInterface.swf \
+-main cw.TestExternalInterface \
+> ./cwtools/testres/build_TestExternalInterface.log
 
-LOG="./cwtools/exp/build_cw_json_tests.log"
-date > $LOG
-echo >> $LOG
-haxe -v -swf-version 9 -swf ./cwtools/exp/cw_json_tests.swf -resource ./hx/cw/json/pass1_modified.json@pass1_modified -swf-header 800:600:1:F7F7F7 -main cw.json.Tests >> $LOG
+haxe -cp hx -v -swf-version 9 -swf \
+./cwtools/exp/cw_json_tests.swf \
+-resource ./hx/cw/json/pass1_modified.json@pass1_modified \
+-swf-header 800:600:1:F7F7F7 \
+-main cw.json.Tests \
+> ./cwtools/exp/build_cw_json_tests.log
