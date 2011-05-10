e490326fb73b9fe2cb1a2abeaee7e6cd22bc6d84
Ivan Kozik <ivan@ludios.org>
2011-05-10 14:08:58 +0000

    Rename .swf build log files to match naming scheme of compiled JS

--- build_swfs.sh
+++ build_swfs.sh
@@ -3,16 +3,16 @@
 haxe -cp hx -v -swf-version 9 -swf \
 ./cwtools/exp/Experiments.swf \
 -main cw.exp.Experiments \
-> ./cwtools/exp/build_Experiments.log
+> ./cwtools/exp/Experiments.swf.log
 
 haxe -cp hx -v -swf-version 9 -swf \
 ./cwtools/testres/TestExternalInterface.swf \
 -main cw.TestExternalInterface \
-> ./cwtools/testres/build_TestExternalInterface.log
+> ./cwtools/testres/TestExternalInterface.swf.log
 
 haxe -cp hx -v -swf-version 9 -swf \
 ./cwtools/exp/cw_json_tests.swf \
 -resource ./hx/cw/json/pass1_modified.json@pass1_modified \
 -swf-header 800:600:1:F7F7F7 \
 -main cw.json.Tests \
-> ./cwtools/exp/build_cw_json_tests.log
+> ./cwtools/exp/cw_json_tests.swf.log
