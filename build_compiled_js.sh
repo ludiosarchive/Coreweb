c36d319aa0eef9b4aff0d7aa17ed0e23b1cfff2a
Ivan Kozik <ivan@ludios.org>
2011-05-12 07:17:30 +0000

    build_compiled_js.sh: Factor out common arguments, in case we have to build more than one .js file later

--- build_compiled_js.sh
+++ build_compiled_js.sh
@@ -1,17 +1,19 @@
 #!/bin/sh -e
 
-../closure-library/closure/bin/build/closurebuilder.py \
+COMMON="../closure-library/closure/bin/build/closurebuilder.py \
 --output_mode=compiled \
 --compiler_jar=../closure-compiler/build/compiler.jar \
---compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" \
---compiler_flags="--warning_level=VERBOSE" \
---compiler_flags="--formatting=PRETTY_PRINT" \
---compiler_flags="--jscomp_warning=missingProperties" \
---compiler_flags="--jscomp_warning=undefinedVars" \
---compiler_flags="--jscomp_warning=checkTypes" \
---compiler_flags="--output_wrapper=(function() { %output%})();" \
---compiler_flags="--summary_detail_level=3" \
---root=../closure-library \
+--compiler_flags=--compilation_level=ADVANCED_OPTIMIZATIONS \
+--compiler_flags=--warning_level=VERBOSE \
+--compiler_flags=--formatting=PRETTY_PRINT \
+--compiler_flags=--jscomp_warning=missingProperties \
+--compiler_flags=--jscomp_warning=undefinedVars \
+--compiler_flags=--jscomp_warning=checkTypes \
+--compiler_flags=--output_wrapper=(function(){%output%})(); \
+--compiler_flags=--summary_detail_level=3 \
+--root=../closure-library"
+
+$COMMON \
 --namespace="cw.tabnexus_worker" \
 --root=js_coreweb \
 --output_file=cwtools/compiled/tabnexus_worker.js \
