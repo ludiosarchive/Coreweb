1f23e9a86a8ee8cbe965b769b8f3a66441c28d64
Ivan Kozik <ivan@ludios.org>
2011-05-12 07:40:03 +0000

    build_compiled_js.sh: minor refactoring

--- build_compiled_js.sh
+++ build_compiled_js.sh
@@ -11,10 +11,11 @@ COMMON="../closure-library/closure/bin/build/closurebuilder.py \
 --compiler_flags=--jscomp_warning=checkTypes \
 --compiler_flags=--output_wrapper=(function(){%output%})(); \
 --compiler_flags=--summary_detail_level=3 \
---root=../closure-library"
+--root=../closure-library \
+--root=js_coreweb \
+"
 
 $COMMON \
 --namespace="cw.tabnexus_worker" \
---root=js_coreweb \
 --output_file=cwtools/compiled/tabnexus_worker.js \
 2>&1 | tee cwtools/compiled/tabnexus_worker.js.log
