2f4c33a38551a8cd86dc6348659d521ab9a2b589
Ivan Kozik <ivan@ludios.org>
2011-05-13 14:16:44 +0000

    cw/__init__.js: Remove obsolete @fileoverview

--- js_coreweb/cw/__init__.js
+++ js_coreweb/cw/__init__.js
@@ -1,9 +1,4 @@
-/**
- * @fileoverview This file was needed to do // import-style imports,
- * 	before we stopped using those.
- */
-
 // This line is useless because it's actually goog.require that creates the
-// necessary `cw` namespace. Its only purpose is to prevent IntelliJ IDEA
+// necessary `cw` namespace.  Its only purpose is to prevent IntelliJ IDEA
 // from displaying squiggly underlines under every usage of `cw.`.
 var cw = cw || {};
