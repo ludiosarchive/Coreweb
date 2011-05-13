bbbe5afbfc38b40229e4d725ba5a9e305897dcce
Ivan Kozik <ivan@ludios.org>
2011-05-13 12:01:35 +0000

    Remove obsolete targets_js.py (used by the unreleased Closurizer daemon)

--- targets_js.py
+++
@@ -1,7 +0,0 @@
-# Note: inputs are relative to JSPATH,
-# output is relative to this file's directory.
-
-targets = [
-	{"inputs": ["cw/tabnexus_worker.js"],
-	"output": "coreweb/compiled/tabnexus_worker.js"},
-]
