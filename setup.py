64a1d24baafe5136a553b1136274ceba8783342a
Ivan Kozik <ivan@ludios.org>
2011-06-07 02:09:40 +0000

    Add classifiers and author info to setup.py; bump __version__

--- setup.py
+++ setup.py
@@ -7,7 +7,21 @@ import coreweb
 setup(
 	name='Coreweb',
 	version=coreweb.__version__,
-	description="Coreweb",
+	description=("JavaScript tools built on Closure Library: test runner "
+		"with Deferred support, Python-like repr and eq, cross-tab "
+		"communication, much more"),
+	url="https://github.com/ludios/Coreweb",
+	author="Ivan Kozik",
+	author_email="ivan@ludios.org",
+	classifiers=[
+		'Programming Language :: JavaScript',
+		'Programming Language :: Python :: 2',
+		'Development Status :: 3 - Alpha',
+		'Operating System :: OS Independent',
+		'Intended Audience :: Developers',
+		'Topic :: Internet :: WWW/HTTP',
+		'License :: OSI Approved :: Apache Software License',
+	],
 	packages=['coreweb', 'twisted.plugins', 'js_coreweb'],
 	package_data={
 		'coreweb': ['*.html', 'testres/*', 'exp/*.html', 'exp/*.swf', 'exp/spinner_behavior/*'],
