927bd8c7952287c091fae4e6111a59a0ef483ccd
Ivan Kozik <ivan@ludios.org>
2011-10-24 21:25:39 +0000

    On Windows, use dos2unix to fix JS build output

--- build_compiled_js.sh
+++ build_compiled_js.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
 
+# Windows users: this script can be run with MSYS bash.
+# Make sure you also have dos2unix.exe.
+
 ./build_depsjs.sh
 
 COMMON="../closure-library/closure/bin/build/closurebuilder.py \
@@ -24,3 +27,10 @@ $COMMON \
 --namespace="cw.tabnexus_worker" \
 --output_file=coreweb/compiled/tabnexus_worker.js \
 2>&1 | tee coreweb/compiled/tabnexus_worker.js.log
+
+UNAME_O=`uname -o`
+if [[ $UNAME_O == 'Msys' || $UNAME_O == 'Cygwin' ]]; then
+	echo "Fixing newlines..."
+	dos2unix coreweb/compiled/tabnexus_worker.js
+	dos2unix coreweb/compiled/tabnexus_worker.js.log
+fi
