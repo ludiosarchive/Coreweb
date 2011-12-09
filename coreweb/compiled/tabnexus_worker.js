fd97c29e5f18e0fd5bffa6c5b398424aa7897c31
Ivan Kozik <ivan@ludios.org>
2011-12-09 14:29:15 +0000

    Rebuild JS with newer Library/Compiler

--- coreweb/compiled/tabnexus_worker.js
+++ coreweb/compiled/tabnexus_worker.js
@@ -3,35 +3,34 @@ function g(a, c) {
   var b = a.split("."), d = f;
   !(b[0] in d) && d.execScript && d.execScript("var " + b[0]);
   for(var e;b.length && (e = b.shift());) {
-    !b.length && c !== void 0 ? d[e] = c : d = d[e] ? d[e] : d[e] = {}
+    !b.length && void 0 !== c ? d[e] = c : d = d[e] ? d[e] : d[e] = {}
   }
 }
 function h(a) {
   var c = typeof a;
-  if(c == "object") {
+  if("object" == c) {
     if(a) {
       if(a instanceof Array) {
         return"array"
-      }else {
-        if(a instanceof Object) {
-          return c
-        }
+      }
+      if(a instanceof Object) {
+        return c
       }
       var b = Object.prototype.toString.call(a);
-      if(b == "[object Window]") {
+      if("[object Window]" == b) {
         return"object"
       }
-      if(b == "[object Array]" || typeof a.length == "number" && typeof a.splice != "undefined" && typeof a.propertyIsEnumerable != "undefined" && !a.propertyIsEnumerable("splice")) {
+      if("[object Array]" == b || "number" == typeof a.length && "undefined" != typeof a.splice && "undefined" != typeof a.propertyIsEnumerable && !a.propertyIsEnumerable("splice")) {
         return"array"
       }
-      if(b == "[object Function]" || typeof a.call != "undefined" && typeof a.propertyIsEnumerable != "undefined" && !a.propertyIsEnumerable("call")) {
+      if("[object Function]" == b || "undefined" != typeof a.call && "undefined" != typeof a.propertyIsEnumerable && !a.propertyIsEnumerable("call")) {
         return"function"
       }
     }else {
       return"null"
     }
   }else {
-    if(c == "function" && typeof a.call == "undefined") {
+    if("function" == c && "undefined" == typeof a.call) {
       return"object"
     }
   }
@@ -44,29 +43,28 @@ function j(a, c, b) {
   if(!a) {
     throw Error();
   }
-  if(arguments.length > 2) {
+  if(2 < arguments.length) {
     var d = Array.prototype.slice.call(arguments, 2);
     return function() {
       var b = Array.prototype.slice.call(arguments);
       Array.prototype.unshift.apply(b, d);
       return a.apply(c, b)
     }
-  }else {
-    return function() {
-      return a.apply(c, arguments)
-    }
+  }
+  return function() {
+    return a.apply(c, arguments)
   }
 }
 function k(a, c, b) {
-  k = Function.prototype.bind && Function.prototype.bind.toString().indexOf("native code") != -1 ? i : j;
+  k = Function.prototype.bind && -1 != Function.prototype.bind.toString().indexOf("native code") ? i : j;
   return k.apply(null, arguments)
 }
 ;var l = Array.prototype, m = l.indexOf ? function(a, c, b) {
   return l.indexOf.call(a, c, b)
 } : function(a, c, b) {
-  b = b == null ? 0 : b < 0 ? Math.max(0, a.length + b) : b;
-  if(typeof a == "string") {
-    return typeof c != "string" || c.length != 1 ? -1 : a.indexOf(c, b)
+  b = null == b ? 0 : 0 > b ? Math.max(0, a.length + b) : b;
+  if("string" == typeof a) {
+    return"string" != typeof c || 1 != c.length ? -1 : a.indexOf(c, b)
   }
   for(;b < a.length;b++) {
     if(b in a && a[b] === c) {
@@ -83,11 +81,11 @@ function n(a, c) {
 }
 n.prototype.f = function(a) {
   a = a.data;
-  if(h(a) == "array" && a[0] == "dying") {
+  if("array" == h(a) && "dying" == a[0]) {
     var c = a[1];
     this.b.close();
     var a = this.d, b = a.a == this, d = a.c, e = m(d, this);
-    e >= 0 && l.splice.call(d, e, 1);
+    0 <= e && l.splice.call(d, e, 1);
     if(b) {
       if(a.c.length) {
         a.a = a.c[0];
